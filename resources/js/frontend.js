8db3829e63592b54fd44b98e58b1187ae3941213
fathergasc <quaglioniandrea@gmail.com>
2022-10-17 15:05:59 +0200

    add: MyHeader component

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -1,5 +1,8 @@
 window.Vue = require("vue");
 
+//alternative method to import Vue
+//import Vue from 'vue'
+
 import App from "./views/App";
 
 const app = new Vue({
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
+    <MyHeader/>
     <h1>Work in progress</h1>
-    <h2>Sito in costruzione</h2>
     <a href="/admin">Go to back-office</a>
   </div>
 </template>
 
 <script>
+import MyHeader from '../components/MyHeader.vue';
+
 export default {
     name: 'App',
     components: {
-
+        MyHeader
+    },
+    data() {
+        return {
+        }
     }
 }
 </script>
